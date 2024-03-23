0ab1f0999f1183ddfc90741050898b71951898b4
Massil <massilaitabdeslam@gmail.com>
2024-03-23 17:57:23 -0400

    ajout du fichier de simulation pour le generateur_sample

--- hdl/generateur_sample.vhd
+++ hdl/generateur_sample.vhd
@@ -15,14 +15,158 @@ entity generateur_sample is
 end generateur_sample;
 
 architecture Behavioral of generateur_sample is
-
-  -- A completer
-
+    type state is(enable_wait, ready_wait,p1,p2,p3,p4);
+    signal current_state, next_state:state;
+    signal p_cnt:integer range 0 to 3:=0;
+    signal addr_cnt : integer range 0 to 4095:=0;
 begin
 
-  -- A remplacer
+  --changement d'etat et reset
+  process(clk_i,rst_i)
+  begin
+    if rst_i='1' then
+        current_state<=enable_wait;
+    elsif rising_edge(clk_i) then
+        current_state<=next_state;
+     
+    end if;
+  
+  end process;
+  
+  -- transitions
+  process(current_state,enable_i,sample_ready_i)
+  begin
+    case current_state is
+        when enable_wait =>
+        
+            if enable_i='1' then
+                p_cnt<=0;
+                addr_cnt<=to_integer(unsigned(note_start_addr_i));
+                next_state<=ready_wait;
+            else
+                next_state<=enable_wait;
+            end if;
+            
+        when ready_wait =>
+            if enable_i='0' then
+                next_state <=enable_wait;
+            elsif sample_ready_i='1' then 
+                case p_cnt is
+                    when 0 =>
+                        next_state <=p1;
+                    when 1=>
+                        next_state <=p2;
+                    when 2=>
+                        next_state <=p3;
+                    when 3=>
+                        next_state <=p3;
+                    when others =>
+                        next_state <=p1;
+                end case;
+                   
+            
+            else
+                next_state<=ready_wait;
+            end if;
+            
+        
+        when p1 =>
+            if enable_i='0' then 
+                next_state<=enable_wait;
+            else 
+                next_state<=ready_wait;
+            end if;
+        
+        when p2 =>
+            if enable_i='0' then 
+                next_state<=enable_wait;
+            else 
+                next_state<=ready_wait;
+            end if;
+        
+        when p3 =>
+        
+            if enable_i='0' then 
+                next_state<=enable_wait;
+            else 
+                next_state<=ready_wait;
+            end if;
+        
+        when p4 =>
+            if enable_i='0' then 
+                next_state<=enable_wait;
+            else 
+                next_state<=ready_wait;
+            end if;
+        
+        when others =>
+            next_state<=enable_wait;
+    end case;
+  
+  
+  end process;
+  
+  
+  -- output
+  process(current_state)
+  begin
+    case current_state is
+        when enable_wait =>        
+            ROM_qsin_addr_o<=std_logic_vector(to_unsigned(addr_cnt,12));
+            sample_out<=(16 downto 0=>'0') and ROM_qsin_sample_i;
+            
+        when ready_wait =>
+            ROM_qsin_addr_o<=std_logic_vector(to_unsigned(addr_cnt,12));
+            case p_cnt is
+                    when 0 =>
+                        sample_out<=(16 downto 0=>'0') and ROM_qsin_sample_i;
+                    when 1=>
+                        sample_out<=(16 downto 0=>'0') and ROM_qsin_sample_i;
+                    when 2=>
+                        sample_out<=std_logic_vector(not(unsigned((16 downto 0=>'0') and ROM_qsin_sample_i))+1);
+                    when 3=>
+                        sample_out<=std_logic_vector(not(unsigned((16 downto 0=>'0') and ROM_qsin_sample_i))+1);        
+                end case;
+        
+        when p1 =>
+            if addr_cnt<unsigned(note_start_addr_i)+unsigned(note_sample_count_i)-1 then 
+                addr_cnt<=addr_cnt+1;
+            else
+                p_cnt<=1;
+            end if;
+           
+        
+        when p2 =>
+            if addr_cnt>unsigned(note_start_addr_i) then 
+                addr_cnt<=addr_cnt-1;
+            else
+                p_cnt<=2;
+            end if;
+        
+        when p3 =>
+            if addr_cnt<unsigned(note_start_addr_i)+unsigned(note_sample_count_i)-1 then 
+                addr_cnt<=addr_cnt+1;
+            else
+                p_cnt<=3;
+            end if;
+           
+        
+        when p4 =>
+            if addr_cnt>unsigned(note_start_addr_i) then 
+                addr_cnt<=addr_cnt-1;
+            else
+                p_cnt<=0;
+            end if;
+            
+        
+        when others =>
+            
+            
+    end case;
+  
+  
+  end process;
 
-  ROM_qsin_addr_o <= X"000";
-  sample_out      <= X"000000";
+  
 
 end Behavioral;
---
+++ sim/generateur_sample_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/23/2024 05:53:31 PM
+-- Design Name: 
+-- Module Name: generateur_sample_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity generateur_sample_tb is
+--  Port ( );
+end generateur_sample_tb;
+
+architecture Behavioral of generateur_sample_tb is
+
+begin
+
+
+end Behavioral;
