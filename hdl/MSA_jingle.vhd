6d3bd80a81cfe2fcf15fb062756348e8ce2e6494
Massil <massilaitabdeslam@gmail.com>
2024-03-24 14:49:20 -0400

    msa_jingle fontionnel

--- hdl/MSA_jingle.vhd
+++ hdl/MSA_jingle.vhd
@@ -15,48 +15,80 @@ end MSA_jingle;
 architecture Behavioral of MSA_jingle is
 
   signal addr_cnt: integer range 0 to 7 :=0;
-  signal jingle_active: boolean:=false;
+  type state is (jingle_wait, jingle_activate,ready_wait, incr);
+  signal current_state, next_state : state;
 
 begin
 
+  --changement d'etat
   process(clk_i, rst_i)
   begin
-  if rst_i='0' then 
+  if rst_i='1' then 
   
-    timing_start_o <= '0';
-    addr_cnt <= 0;
-    jingle_active<=false;
-    note_enable_o<='0';
-
+    current_state<=jingle_wait;
     
   elsif rising_edge(clk_i) then 
-    if jingle_active and addr_cnt<2 then 
-        if timing_ready_i='1' then 
-            note_enable_o<='0';
-            timing_start_o<='1';
-            addr_cnt<=addr_cnt+1;
-        else
-            note_enable_o<='1';
-            timing_start_o<='0';      
-        end if;
-        
-        
-    elsif not jingle_active and start_jingle_i='1' then 
-        jingle_active<=true;
-        
-    else 
-        timing_start_o <= '0';
-        addr_cnt <= 0;
-        jingle_active<=false;
-        note_enable_o<='0';
-        
-    end if; 
+     current_state<=next_state;
+     
   end if;
-  
-    
   
   end process;
   
+  process(current_state,start_jingle_i, timing_ready_i)
+  begin
+    case current_state is 
+        when jingle_wait =>
+            addr_cnt<=0;
+            timing_start_o<='0';
+            note_enable_o <='0';
+            
+            if start_jingle_i='1' then
+                next_state<=jingle_activate;
+            else
+                next_state<=jingle_wait;
+            end if;
+            
+        
+        when jingle_activate =>
+            addr_cnt<=0;
+            timing_start_o<='1';
+            note_enable_o <='1';
+            
+            next_state<=ready_wait;
+        
+        when ready_wait =>    
+            addr_cnt<=addr_cnt;
+            timing_start_o<='0';
+            note_enable_o <='1';
+            
+            if timing_ready_i='1' then
+                next_state<=incr;
+            else
+                next_state<=ready_wait;
+            end if;
+        
+        when incr =>
+            if addr_cnt+1<2 then 
+                addr_cnt<=addr_cnt+1;
+                timing_start_o<='1';
+                note_enable_o <='1';
+                 next_state<=ready_wait;
+            else
+                addr_cnt<=0;
+                timing_start_o<='1';
+                note_enable_o <='1';
+                next_state<=jingle_wait;
+            end if; 
+        
+        when others =>
+            next_state<=jingle_wait;
+        
+    
+    end case;
+  
+  end process;
+  
+  jingle_address_o<=std_logic_vector(to_unsigned(addr_cnt,8));
 
   
 end Behavioral;
--- sim/MSA_jingle_tb.vhd
+++ sim/MSA_jingle_tb.vhd
@@ -19,13 +19,94 @@ architecture bench of MSA_jingle_tb is
    
    signal clk_tb : std_logic :='0';
    signal rst_i_tb : std_logic;
-   signal start_jingle_i_tb : std_logic;
+   signal start_jingle_i_tb : std_logic:='0';
    signal timing_start_o_tb   : std_logic;
    signal timing_ready_i_tb   : std_logic;
    signal jingle_address_o_tb : std_logic_vector(7 downto 0);
    signal note_enable_o_tb    : std_logic;
    
-begin
+   
+   component ROM_jingle
+        port (
+        clk_i                : in  std_logic;
+        addr_i               : in  std_logic_vector(7 downto 0);
+        note_duration_o      : out std_logic_vector(15 downto 0);
+        note_start_address_o : out std_logic_vector(11 downto 0);
+        note_sample_count_o  : out std_logic_vector(7 downto 0)
+        );
+  end component; 
+  
+  signal addr_i_tb               : std_logic_vector(7 downto 0);
+  signal note_duration_o_tb      : std_logic_vector(15 downto 0);
+  signal note_start_address_o_tb : std_logic_vector(11 downto 0);
+  signal note_sample_count_o_tb  : std_logic_vector(7 downto 0);
+  
+  component MSA_timing 
+        port (clk_i   : in  std_logic;
+        rst_i   : in  std_logic;
+        start_i : in  std_logic;
+        data_i  : in  std_logic_vector (15 downto 0);
+        ready_o : out std_logic);
+  end component;
+  
+  
+  signal rst_i_2_tb   : std_logic;
+  signal start_i_tb   : std_logic;
+  signal data_i_tb    : std_logic_vector (15 downto 0);
+  signal ready_o_tb   : std_logic;
+    
+  constant clk_period : time    := 5 ns;  
+begin 
+    uut1:MSA_jingle
+        port map(
+        clk_i  => clk_tb,
+        rst_i  => rst_i_tb,
+        start_jingle_i  => start_jingle_i_tb,
+        timing_start_o  => timing_start_o_tb,
+        timing_ready_i  => timing_ready_i_tb,
+        jingle_address_o => jingle_address_o_tb,
+        note_enable_o   => note_enable_o_tb
+        );
+    
+    uut2: ROM_jingle
+        port map(
+        clk_i  => clk_tb,
+        addr_i   => addr_i_tb,
+        note_duration_o    =>note_duration_o_tb,
+        note_start_address_o => note_start_address_o_tb,
+        note_sample_count_o  => note_sample_count_o_tb
+        );
+        
+        
+    uut3: MSA_timing 
+        port map(
+        clk_i  => clk_tb,
+        rst_i  => rst_i_2_tb,
+        start_i => start_i_tb,
+        data_i  => data_i_tb,
+        ready_o => ready_o_tb
+        );
+    
+    addr_i_tb<=jingle_address_o_tb;
+    timing_ready_i_tb<=ready_o_tb;
+    start_i_tb<=timing_start_o_tb;
+    data_i_tb<=note_duration_o_tb;
+    
+    clk_tb <= not clk_tb after clk_period/2 ;
+    rst_i_tb <= '1', '0' after 5*clk_period;
+    rst_i_2_tb <= '1', '0' after 5*clk_period;
+    
+    process
+    begin
+    wait for 10*clk_period;
+    start_jingle_i_tb<='1';
+    wait for 1*clk_period;
+    start_jingle_i_tb<='0';
+    wait;
+    end process;
+    
+    
+        
 
 
 end bench;
