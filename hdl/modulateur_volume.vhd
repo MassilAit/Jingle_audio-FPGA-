a4bb7edf804992e120dbb133af3b1ae9cd0aff54
Massil <massilaitabdeslam@gmail.com>
2024-04-03 22:46:44 -0400

    modification du modulateur de volume V1

--- hdl/modulateur_volume.vhd
+++ hdl/modulateur_volume.vhd
@@ -19,14 +19,49 @@ end modulateur_volume;
 
 architecture Behavioral of modulateur_volume is
 
-  -- A completer
+  signal v_cnt:integer range 0 to 15:=7;
+  signal sample_in_integer:integer range -8388608 to 8388607;
+  signal column_integer:integer range 0 to 127;
 
 begin
 
-  -- A remplacer
+    process(clk_i, btnd_i, btnu_i)
+    begin
+        if rising_edge(btnd_i) then 
+            start_jingle_o<='1';
+            if v_cnt>0 then 
+                v_cnt<=v_cnt-1;
+            end if;
+         
+        elsif rising_edge(btnu_i) then 
+            start_jingle_o<='1';
+            if v_cnt<15 then 
+                v_cnt<=v_cnt+1;
+            end if;
+    
+        else
+            start_jingle_o<='0';
+    
+        end if;
+    
+    end process;
 
-  sample_out <= sample_in;
-  pixel_o <= '0';
-  start_jingle_o <= btnu_i;
+
+    process(clk_i)
+    begin
+        if (column_integer/8>=v_cnt) then 
+            pixel_o<='1';
+        else 
+            pixel_o<='0';
+       end if;
+    
+    end process;
+
+  
+  column_integer<=to_integer(unsigned(column_i));
+  sample_in_integer<=to_integer(signed(sample_in));
+  sample_out <= std_logic_vector(to_signed(sample_in_integer*v_cnt*2056,24));
+  
+  
 
 end Behavioral;
