4043d2425bb2c3ddbf8f4138a4fb49a0ecaa514f
Massil <massilaitabdeslam@gmail.com>
2024-04-04 07:49:25 -0400

    test bench du modulateur volume

--- sim/modulateur_volume_tb.vhd
+++ sim/modulateur_volume_tb.vhd
@@ -1,43 +1,85 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/03/2024 10:42:22 PM
--- Design Name: 
--- Module Name: modulateur_volume_tb - bench
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity modulateur_volume_tb is
---  Port ( );
 end modulateur_volume_tb;
 
 architecture bench of modulateur_volume_tb is
-
+    component modulateur_volume
+        port (
+        rst_i          : in  std_logic;
+        clk_i          : in  std_logic;
+        btnd_i         : in  std_logic;
+        btnu_i         : in  std_logic;
+        sample_in      : in  std_logic_vector(23 downto 0);
+        sample_out     : out std_logic_vector(23 downto 0);
+        column_i       : in  std_logic_vector(6 downto 0);
+        row_i          : in  std_logic_vector(4 downto 0);
+        pixel_o        : out std_logic;
+        start_jingle_o : out std_logic
+);
+    end component;
+    
+    
+     signal rst_tb            : std_logic;
+     signal clk_tb            : std_logic:='0';
+     signal btnd_i_tb         : std_logic:='0';
+     signal btnu_i_tb         : std_logic:='0';
+     signal sample_in_tb      : std_logic_vector(23 downto 0);
+     signal sample_out_tb     : std_logic_vector(23 downto 0);
+     signal column_i_tb       : std_logic_vector(6 downto 0);
+     signal row_i_tb          : std_logic_vector(4 downto 0);
+     signal pixel_o_tb        : std_logic;
+     signal start_jingle_o_tb : std_logic;
+    
+    
+    constant clk_period : time    := 5 ns;
+            
 begin
-
-
+    uut1:modulateur_volume
+        port map(
+        rst_i          => rst_tb,
+        clk_i          => clk_tb,
+        btnd_i         => btnd_i_tb,
+        btnu_i         => btnu_i_tb,
+        sample_in      => sample_in_tb,
+        sample_out     => sample_out_tb,
+        column_i       => column_i_tb,
+        row_i          => row_i_tb,
+        pixel_o        => pixel_o_tb,
+        start_jingle_o => start_jingle_o_tb
+        );
+    
+    
+    clk_tb <= not clk_tb after clk_period/2 ;
+    
+    process
+    begin
+        sample_in_tb<=std_logic_vector(to_signed(-200,24));
+        row_i_tb<="00000";
+        column_i_tb<="1000100";
+        wait for 5*clk_period;
+        
+        btnd_i_tb<='1';
+        wait for 5*clk_period;
+        btnd_i_tb<='0';
+        
+        
+        wait for 10*clk_period;
+        
+        btnu_i_tb<='1';
+        wait for 5*clk_period;
+        btnu_i_tb<='0';
+        wait for 10*clk_period;
+        
+        btnu_i_tb<='1';
+        wait for 5*clk_period;
+        btnu_i_tb<='0';
+        wait for 10*clk_period;
+    
+    
+    wait;
+    end process;
 end bench;
